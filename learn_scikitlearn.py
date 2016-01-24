fa967ee64e3769000a09cb1b1386845c8fe3b0cb
Aravind Rao <aravind@oxidification.com>
2016-01-24 21:25:59 +0530

    Move report  to root folder for convenience

--- learn_scikitlearn.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pylab as pl
-from sklearn import datasets
-
-a = np.array(((1, 2, 3),(4, 5, 6)), dtype=np.float32)
-print "Array =", a
-print "Mean =", a.mean()
-print "Std =", a.std()
-boston = datasets.load_boston()
